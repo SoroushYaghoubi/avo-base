3ac8fd7e02f1cf81db2dcf0617cffbf7cc651514
soroush.yaghoubi <soroush.yag@gmail.com>
2023-07-02 12:31:45 +0200

    calendar-base module created. avo-month declared.

---
+++ base/src/app/calendar-base/avo-month/avo-month.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avo-month',
+  templateUrl: './avo-month.component.html',
+  styleUrls: ['./avo-month.component.css']
+})
+export class AvoMonthComponent {
+
+}
---
+++ base/src/app/calendar-base/calendar-base.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AvoMonthComponent } from './avo-month/avo-month.component';
+
+
+
+@NgModule({
+  declarations: [
+    AvoMonthComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalendarBaseModule { }
