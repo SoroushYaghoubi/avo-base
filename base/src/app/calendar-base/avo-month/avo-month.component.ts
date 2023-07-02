ac8aa26c53a452c52273d5674db28b19ed0b063f
soroush.yaghoubi <soroush.yag@gmail.com>
2023-07-03 00:07:40 +0200

    the calendar is built, and needs to be adjusted.

--- base/src/app/calendar-base/avo-month/avo-month.component.ts
+++ base/src/app/calendar-base/avo-month/avo-month.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-avo-month',
+  selector: 'avo-month',
   templateUrl: './avo-month.component.html',
   styleUrls: ['./avo-month.component.css']
 })
 export class AvoMonthComponent {
-
-}
+  selected: Date | null;
+  startView: 'month' | 'year' | 'multi-year' = 'month';
+  startAt: Date = new Date();
+} 
--- base/src/app/calendar-base/calendar-base.module.ts
+++ base/src/app/calendar-base/calendar-base.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvoMonthComponent } from './avo-month/avo-month.component';
 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 
 @NgModule({
@@ -9,7 +13,13 @@ import { AvoMonthComponent } from './avo-month/avo-month.component';
     AvoMonthComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+    MatButtonToggleModule
   ],
   exports:[
     AvoMonthComponent
